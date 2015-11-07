664fe4eb040e18cd997b2c5471fde0862bc52847
Igor Babko <i.i.babko@gmail.com>
2015-11-07 20:41:01 +0200

    feature(uploads): create file management functionality in admin panel

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -9,11 +9,11 @@
 | It's a breeze. Simply tell Laravel the URIs it should respond to
 | and give it the controller to call when that URI is requested.
 |
-*/
+ */
 
 // Blog pages
 get('/', function () {
-	return redirect('/posts');
+    return redirect('/posts');
 });
 
 get('/bio', 'PagesController@showBio');
@@ -25,20 +25,28 @@ get('/posts/{slug}', 'PagesController@showPost');
 
 // Admin area
 get('admin', function () {
-	return redirect('/admin/post');
+    return redirect('/admin/post');
 });
 
 $router->group([
-		'namespace' => 'Admin',
-		'middleware' => 'auth',
-	], function () {
-		resource('admin/post', 'PostController');
-		resource('admin/tag', 'TagController', ['except' => 'show']);
-		get('admin/upload', 'UploadController@index');
-	}
+    'namespace' => 'Admin',
+    'middleware' => 'auth',
+], function () {
+    resource('admin/post', 'PostController');
+    resource('admin/tag', 'TagController', ['except' => 'show']);
+    get('admin/upload', 'UploadController@index');
+}
 );
 
 // Logging in and out
 get('/auth/login', 'Auth\AuthController@getLogin');
 post('/auth/login', 'Auth\AuthController@postLogin');
 get('/auth/logout', 'Auth\AuthController@getLogout');
+
+get('admin/upload', 'Admin\UploadController@index');
+
+// Add the following routes
+post('admin/upload/file', 'Admin\UploadController@uploadFile');
+delete('admin/upload/file', 'Admin\UploadController@deleteFile');
+post('admin/upload/folder', 'Admin\UploadController@createFolder');
+delete('admin/upload/folder', 'Admin\UploadController@deleteFolder');
