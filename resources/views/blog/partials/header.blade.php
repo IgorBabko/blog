ff49af0bda98e1ab8584bcb7a2aa69e444c52429
Igor Babko <i.i.babko@gmail.com>
2015-12-01 00:13:50 +0200

    add 'Open Sans' Google font

--- resources/views/blog/partials/header.blade.php
+++ resources/views/blog/partials/header.blade.php
@@ -1,4 +1,4 @@
 <header>
-    <h1 id="welcome">Blog is dedicated for those who's looking to improve their programming skills and just loves to &lt;code/&gt; <br><span style="display: inline-block; font-size: 100px; font-weight: 200; transform: rotate(90deg)">:)</span></h1>
+    <h1 id="welcome">My blog is dedicated for those who's looking to improve their programming skills and just loves to <span style="color: #FF0000">&lt;</span><span style="color: #F60">c</span><span style="color: #FE0">o</span><span style="color: #0F0">d</span><span style="color: #09F">e</span><span style="color: #40F">/</span><span style="color: #90F">&gt;</span> <br><span style="display: inline-block; font-size: 100px; font-weight: 200;">:)</span></h1>
     @include('blog.partials.nav')
 </header>
