1be42f9cac009c470ea892e06f99f9646330c0fd
Igor Babko <i.i.babko@gmail.com>
2015-12-01 18:28:43 +0200

    style category list

--- resources/views/blog/sections/posts.blade.php
+++ resources/views/blog/sections/posts.blade.php
@@ -3,38 +3,62 @@
 	<div class="section-content">
 		<div id="categories">
 			<ul id="categories-list">
-				<li class="active-category-item">
-					<a id="none" href="posts?page=1">
-						<img src="/assets/img/code.png">
-					<a>
-				</li>
 				<li>
-					<a id="1" href="category/1/posts?page=1">
-						<img src="/assets/img/code.png">
-					<a>
-				</li>
-				<li>
-					<a id="2" href="category/2/posts?page=1">
-						<img style="width: 100px;" src="/assets/img/code.png">
+					<a id="1" href="category/2/posts?page=1">
+						<figure>
+							<div class="category-img-wrapper">
+								<img src="/assets/img/laravel.png" alt="laravel">
+							</div>
+							<figcaption>Laravel</figcaption>
+						</figure>
 					</a>
 				</li>
 				<li>
-					<a id="3" href="category/3/posts?page=1">
-						<img src="/assets/img/webdev.png">
+					<a id="2" href="category/3/posts?page=1">
+						<figure>
+							<div class="category-img-wrapper">
+								<img src="/assets/img/nodejs.png" alt="nodejs">
+							</div>
+							<figcaption>NodeJS</figcaption>
+						</figure>
 					</a>
 				</li>
-				{{-- <li id="1">
-					<figure>
-						<img style="width: 100px;" src="/assets/img/code.png">
-						<figcaption >Programming</figcaption>
-					</figure>
-				</li> --}}
+				<li>
+					<a id="3" href="category/1/posts?page=1">
+						<figure>
+							<div class="category-img-wrapper">
+								<img src="/assets/img/meteor.png" alt="meteorjs">
+							</div>
+							<figcaption>MeteorJS</figcaption>
+						</figure>
+					</a>
+				</li>
+				<li>
+					<a id="4" href="category/1/posts?page=1">
+						<figure>
+							<div class="category-img-wrapper">
+								<img src="/assets/img/jquery.png" alt="jquery">
+							</div>
+							<figcaption>jQuery</figcaption>
+						</figure>
+					</a>
+				</li>
+				<li>
+					<a id="4" href="category/1/posts?page=1">
+						<figure>
+							<div class="category-img-wrapper">
+								<img src="/assets/img/jquery.png" alt="jquery">
+							</div>
+							<figcaption>jQuery</figcaption>
+						</figure>
+					</a>
+				</li>
 			</ul>
 		</div>
-		<div class="flex-block">
+		<!-- <div class="flex-block">
 			<input type="text" id="search-posts-input" class="form-control search-input" placeholder="Search for post:" >
 			<button id="search-posts-button" class="button inline-button"><i class="fa fa-search"></i></button>
-		</div>
+		</div> -->
 		<div id="posts-block">
 			@include("blog.partials.posts-block")
 		</div>
