79db366dc6d6fb45e210b695dd3fecc784f95fff
Igor Babko <i.i.babko@gmail.com>
2015-12-06 00:32:18 +0200

    change main font color

--- resources/views/blog/sections/about.blade.php
+++ resources/views/blog/sections/about.blade.php
@@ -1,6 +1,44 @@
 <section id="about-section">
     <h1 class="section-title">About me</h1>
     <div class="section-content">
+        <div id="myCarousel" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+    <li data-target="#myCarousel" data-slide-to="1"></li>
+    <li data-target="#myCarousel" data-slide-to="2"></li>
+    <li data-target="#myCarousel" data-slide-to="3"></li>
+  </ol>
+
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner" role="listbox">
+    <div class="item active">
+      <img src="/assets/img/1.jpeg" alt="Chania">
+    </div>
+
+    <div class="item">
+      <img src="/assets/img/2.jpg" alt="Chania">
+    </div>
+
+    <div class="item">
+      <img src="/assets/img/3.jpg" alt="Flower">
+    </div>
+
+    <div class="item">
+      <img src="/assets/img/2.jpg" alt="Flower">
+    </div>
+  </div>
+
+  <!-- Left and right controls -->
+  <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
         <img id="me-img" src="/assets/img/me-portrait.jpg">
         <ul class="nav nav-tabs nav-justified">
             <li><a href="#about-me" data-toggle="tab">About me</a></li>
